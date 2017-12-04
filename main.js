2521791465676d5d83b1f8890afb18cf6e9a4d5f
Eli Jackson <EliJackson@Elis-MacBook-Pro.local>
2017-12-04 09:26:45 -0600

    completed exercise

--- main.js
+++ main.js
@@ -16,7 +16,25 @@ var people = [
         title: "Hardware guy"
     },
     {
-        name: "Glenda": 
+        name: "Glenda",
         department: "C-suite", 
         title: "CEO"}
 ]
+
+for (let i = 0; i < name.length; i++) {
+    console.log(`${name[i].name}'s job title is ${name[i].title}`);
+}
+
+// for (var i=0; i < people.length; i++) {
+//     if (i === 0)
+//     document.getElementById("Bob").innerHTML+="<p>" + name[i].name + "'s" + " job title is " + name[i].title
+// }
+//     else if (i === 1) {
+//     document.getElementById("Tina").innerHTML+="<p>" + name[i].name + "'s" + " job title is " + name[i].title
+// }
+//     else if (i === 2) {
+//     document.getElementById("Randy").innerHTML+="<p>" + name[i].name + "'s" + " job title is " + name[i].title
+// }
+//     else if (i === 3) {
+//     document.getElementById("Bob").innerHTML+="<p>" + name[i].name + "'s" + " job title is " + name[i].title
+// }
