020a6a066443d73b5b99ec22bb2a0876cf59a697
Eli Jackson <EliJackson@Elis-MacBook-Pro.local>
2017-12-04 09:16:00 -0600

    created array for first ex

---
+++ main.js
@@ -0,0 +1,22 @@
+var people = [
+    {
+        name: "Bob", 
+        department: "sales", 
+        title: "sales manager"
+    },
+    {
+        name: "Tina", 
+        department: "finance", 
+        title: "director of finance"
+    },
+
+    {
+        name: "Randy", 
+        department: "IT",
+        title: "Hardware guy"
+    },
+    {
+        name: "Glenda": 
+        department: "C-suite", 
+        title: "CEO"}
+]
