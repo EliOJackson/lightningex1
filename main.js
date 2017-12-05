60da58f79ff6b65f6e5a6c152962b2ada45654c0
Eli Jackson <EliJackson@Elis-MacBook-Pro.local>
2017-12-05 13:30:20 -0600

    second lightning exercise commit

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
-var people = [
-    {
-        name: "Bob", 
-        department: "sales", 
-        title: "sales manager"
-    },
-    {
-        name: "Tina", 
-        department: "finance", 
-        title: "director of finance"
-    },
+// var people = [
+//     {
+//         name: "Bob", 
+//         department: "sales", 
+//         title: "sales manager"
+//     },
+//     {
+//         name: "Tina", 
+//         department: "finance", 
+//         title: "director of finance"
+//     },
 
-    {
-        name: "Randy", 
-        department: "IT",
-        title: "Hardware guy"
-    },
-    {
-        name: "Glenda",
-        department: "C-suite", 
-        title: "CEO"}
-]
+//     {
+//         name: "Randy", 
+//         department: "IT",
+//         title: "Hardware guy"
+//     },
+//     {
+//         name: "Glenda",
+//         department: "C-suite", 
+//         title: "CEO"}
+// ]
 
-for (let i = 0; i < people.length; i++) {
-    console.log(`${people[i].name}'s job title is ${people[i].title}`);
-};
+// for (let i = 0; i < people.length; i++) {
+//     console.log(`${people[i].name}'s job title is ${people[i].title}`);
+// };
 
 // for (var i=0; i < people.length; i++) {
 //     if (i === 0)
@@ -38,3 +38,25 @@ for (let i = 0; i < people.length; i++) {
 //     else if (i === 3) {
 //     document.getElementById("Bob").innerHTML+="<p>" + name[i].name + "'s" + " job title is " + name[i].title
 // }
+
+
+  let lunch = { 
+    
+        name: "Peanut Butter",
+        type: "Sandwhich",
+        bread: "wheat",
+        fullName: function() {
+            return lunch.name + " " + lunch.type
+    }
+};
+
+console.log(lunch);
+
+    let dinner = Object.create(lunch);
+    dinner.time = "7:30 PM";
+    dinner.location = "The Kitchen Table";
+
+console.log(dinner);
+console.log(dinner.hasOwnProperty("name"));
+console.log(dinner.hasOwnProperty("time"));
+        
