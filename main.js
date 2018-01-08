4f373f9efef04a50b5b61585a577bb0d5890e5be
Eli Jackson <tucker.eli.jackson@gmail.com>
2018-01-08 10:00:09 -0600

    lightning ex for 1/8/18

--- main.js
+++ main.js
@@ -140,13 +140,66 @@ let fave_demo = [];
 
 // 1/4/18
 // Choose a loop method of your choice to get this result from this array:
-// expected result: [3, 5, 7, 9, 11, 13]
-let nums = [1, 2, 3, 4, "5", 6, "7"]
-for (let i = 0; i < nums.length; i++) {
-    let newNums = nums[i] + ;
-    console.log(newNums);
+// // expected result: [3, 5, 7, 9, 11, 13]
+// let nums = [1, 2, 3, 4, "5", 6, "7"]
+// for (let i = 0; i < nums.length; i++) {
+//     let newNums = nums[i] + ;
+//     console.log(newNums);
+// }
+// let result = [];
+// for (let i = 0; i < nums.length-1; i++) {
+//     result.push(+nums[i] + +nums[i + 1]);
+// }
+
+// 1 / 8 / 18
+// lightning exercise:
+// Loop through this object and add each member object's key as an ID. Log the results.
+let customers = {
+    active: {
+      a87b6345: {
+        name: "Gary Harrison",
+        age: 23,
+        member_level: "gold"
+      },
+      b345hy66: {
+        name: "Harry Garrison",
+        age: 34,
+        member_level: "silver"
+      },
+      nh56yht8: {
+        name: "Mary Terryson",
+        age: 78,
+        member_level: "platinum"
+      },
+      j677yd456: {
+        name: "Barry Sonnyman",
+        age: 44,
+        member_level: "gold"
+      }
+    },
+    inactive: {
+      ur56tfgt: {
+        name: "Barb Justice",
+        age: 56,
+        member_level: "none"
+      },
+      d56gt6: {
+        name: "Jane L'eggo",
+        age: 11,
+        member_level: "none"
+      }
+    }
 }
-let result = [];
-for (let i = 0; i < nums.length-1; i++) {
-    result.push(+nums[i] + +nums[i + 1]);
-}
+
+  console.log(Object.keys(customers));
+let properties = Object.keys(customers);
+console.log(Object.keys(properties));
+  
+    for (let status in customers) {
+        let keys = Object.keys(customers[status]) // making an array out of indiviual keys in customers
+        keys.forEach( (key) => {
+            customers[status][key].id = key;
+        });
+
+    }
+    console.log(customers);
